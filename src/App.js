0ca2a0eb89421af8d0f5bf4eb37fa4bfee6003ed
MathBatistela <matheusbatistela@alunos.utfpr.edu.br>
2019-05-27 10:00:26 -0300

    att

--- src/App.js
+++ src/App.js
@@ -6,14 +6,51 @@ import PrimeiroComponente from './components/PrimeiroComponente/PrimeiroComponen
 export class App extends Component {
 
   state  = {
-    nome: "Matheus"
+    nome: "Matheus",
+    sobrenome: "Batistela"
+  }
+
+  componentWillMount() {
+    this.setState({
+      nome: "Matheus"
+    })
+  }
+
+  componentDidMount(){
+    console.log("fui montado ( ͡~ ͜ʖ ͡°) ")
+  }
+
+  shouldComponentUpdate(nextProps,nextState){
+    if (nextState.sobrenome === "Batistela"){
+      return false
+    }
+    return true
+  }
+
+  updateName = () => {
+    this.setState({
+      nome:"Theuzinho"
+    })
+  }
+
+  updateLastName = () => {
+    this.setState({
+      sobrenome: "DZN"
+    })
   }
 
   render() {
+    const Componente = this.state.nome !== " " ? <p>Tenho nome</p> : <p>Não tenho nome</p>
     return (
       <div>
-        <h1>Oi {this.state.nome}</h1>
+        {/* <h1>Oi {this.state.nome}</h1>
         <PrimeiroComponente nome="teste"/>
+        <PrimeiroComponente nome="testa"/>
+        <PrimeiroComponente nome="testo"/>
+        {Componente} */}
+        <h1>Oi {this.state.nome} {this.state.sobrenome}</h1>
+        <button onClick={this.updateName}>Atualizar nome</button>
+        <button onClick={this.updateLastName}>Atualizar sobrenome</button>
       </div>
     )
   }
--- src/components/PrimeiroComponente/PrimeiroComponente.js
+++ src/components/PrimeiroComponente/PrimeiroComponente.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export class PrimeiroComponente extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Meu PrimeiroComponente</h1>
-                <h3>{this.props.nome}</h3>
-            </div>
-        )
-    }
+// export class PrimeiroComponente extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Meu PrimeiroComponente</h1>
+//                 <h3>{this.props.nome}</h3>
+//             </div>
+//         )
+//     }
+// }
+
+// export default PrimeiroComponente
+
+import React from 'react'
+
+export default function PrimeiroComponente(props) {
+    return (
+        <div>
+            <h2>{props.nome}</h2>
+        </div>
+    )
 }
-
-export default PrimeiroComponente
